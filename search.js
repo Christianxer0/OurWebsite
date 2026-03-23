035cea30d0a51886b4e63baa5b81ec42d1bf3a02
Christianxer0 <christianleano434@gmail.com>
2026-03-23 23:51:05 +0800

    Add some functionality to the site

---
+++ search.js
@@ -0,0 +1,7 @@
+function search() {
+    let input = document.getElementById("search").value.toLowerCase().trim();
+
+    if (input === "ourwebsite") {
+        window.location.href = "https://christianxer0.github.io/OurWebsite/";
+    }
+}
