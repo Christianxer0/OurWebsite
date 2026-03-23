8f81de643c819ae16ffdce1e958733767c653eb4
Christianxer0 <christianleano434@gmail.com>
2026-03-24 00:12:06 +0800

    Remove search.js and update index.html

--- search.js
+++
@@ -1,7 +0,0 @@
-function search() {
-    let input = document.getElementById("search").value.toLowerCase().trim();
-
-    if (input === "ourwebsite") {
-        window.location.href = "https://christianxer0.github.io/OurWebsite/";
-    }
-}
